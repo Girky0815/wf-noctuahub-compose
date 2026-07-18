2325f6be90dd4b44346b50230a477385a73780cf
Girky0815 <rmkb0427@gmail.com>
2026-07-18 23:03:57 +0900

    自動生成

--- composeApp/src/commonMain/kotlin/jp/girky/wf_noctuahub/platform/LibraryVersions.kt
+++ composeApp/src/commonMain/kotlin/jp/girky/wf_noctuahub/platform/LibraryVersions.kt
@@ -2,12 +2,12 @@ package jp.girky.wf_noctuahub.platform
 
 object LibraryVersions {
     const val AGP = "9.2.1"
-    const val COMPOSE = "1.12.0-alpha02"
+    const val COMPOSE = "1.12.0-beta02"
     const val KOTLIN = "2.4.0"
-    const val KTOR = "3.5.0"
+    const val KTOR = "3.5.1"
     const val SERIALIZATION = "1.11.0"
     const val DATETIME = "0.8.0"
-    const val MATERIAL_KOLOR = "5.0.0-alpha07"
+    const val MATERIAL_KOLOR = "5.0.0"
     const val JNA = "5.19.1"
     const val TUKAANI_XZ = "1.12"
     const val SETTINGS = "1.3.0"
